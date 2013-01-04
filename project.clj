c91d64eca99b4ccff8000108e21d96eb0b3325e0
Peter Taoussanis <ptaoussanis@gmail.com>
2013-01-04 15:03:27 +0700

    Bump version (minor)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/timbre "1.1.0"
+(defproject com.taoensso/timbre "1.2.0"
   :description "Clojure logging & profiling library"
   :url "https://github.com/ptaoussanis/timbre"
   :license {:name "Eclipse Public License"}
