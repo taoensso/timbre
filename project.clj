91b18f0583ec871cf53930671fd755ebe8eab78b
Emlyn Corrin <emlyn@swiftkey.net>
2013-05-09 10:33:32 +0100

    Socket appender

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject com.taoensso/timbre "1.6.0"
+(defproject emlyn/timbre "1.6.1"
   :description "Clojure logging & profiling library"
-  :url "https://github.com/ptaoussanis/timbre"
+  :url "https://github.com/emlyn/timbre"
   :license {:name "Eclipse Public License"}
-  :dependencies [[org.clojure/clojure     "1.3.0"]
+  :dependencies [[org.clojure/clojure     "1.5.1"]
                  [org.clojure/tools.macro "0.1.1"]
                  [clj-stacktrace          "0.2.5"]]
   :profiles {:1.3  {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5  {:dependencies [[org.clojure/clojure "1.5.0-alpha3"]]}
+             :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :dev  {:dependencies []}
              :test {:dependencies []}}
   :aliases {"test-all" ["with-profile" "test,1.3:test,1.4:test,1.5" "test"]}
