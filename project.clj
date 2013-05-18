cef5440d50e9d21b97672e375fa8390ca831043e
Peter Taoussanis <ptaoussanis@gmail.com>
2013-05-18 16:54:29 +0700

    Bump Clojure 1.5 test dependency (1.5.1)

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [clj-stacktrace          "0.2.5"]]
   :profiles {:1.3  {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5  {:dependencies [[org.clojure/clojure "1.5.0-alpha3"]]}
+             :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :dev  {:dependencies []}
              :test {:dependencies []}}
   :aliases {"test-all" ["with-profile" "test,1.3:test,1.4:test,1.5" "test"]}
