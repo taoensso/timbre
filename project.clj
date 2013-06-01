2c6939a3271b2ad12cb97a068e0470c6030e29f9
Peter Taoussanis <ptaoussanis@gmail.com>
2013-06-01 19:36:31 +0700

    project.clj housekeeping, add Codox as plugin

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject com.taoensso/timbre "2.0.0"
   :description "Clojure logging & profiling library"
   :url "https://github.com/ptaoussanis/timbre"
-  :license {:name "Eclipse Public License"}
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure     "1.4.0"]
                  [org.clojure/tools.macro "0.1.1"]
                  [clj-stacktrace          "0.2.5"]]
@@ -10,5 +11,6 @@
              :dev  {:dependencies []}
              :test {:dependencies []}}
   :aliases {"test-all" ["with-profile" "test,1.4:test,1.5" "test"]}
+  :plugins [[codox "0.6.4"]]
   :min-lein-version "2.0.0"
   :warn-on-reflection true)
