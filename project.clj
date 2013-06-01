0dea5a430bdab7fef0204aed63e51d2abf58bb31
Peter Taoussanis <ptaoussanis@gmail.com>
2013-06-01 19:31:14 +0700

    BREAKING: Bump minimum Clojure version (1.4)

--- project.clj
+++ project.clj
@@ -2,14 +2,13 @@
   :description "Clojure logging & profiling library"
   :url "https://github.com/ptaoussanis/timbre"
   :license {:name "Eclipse Public License"}
-  :dependencies [[org.clojure/clojure     "1.3.0"]
+  :dependencies [[org.clojure/clojure     "1.4.0"]
                  [org.clojure/tools.macro "0.1.1"]
                  [clj-stacktrace          "0.2.5"]]
-  :profiles {:1.3  {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
+  :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :dev  {:dependencies []}
              :test {:dependencies []}}
-  :aliases {"test-all" ["with-profile" "test,1.3:test,1.4:test,1.5" "test"]}
+  :aliases {"test-all" ["with-profile" "test,1.4:test,1.5" "test"]}
   :min-lein-version "2.0.0"
   :warn-on-reflection true)
