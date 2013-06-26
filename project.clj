7050f7d205ddf15de4d7cf251498390f0001ba0b
Peter Taoussanis <ptaoussanis@gmail.com>
2013-06-26 18:34:55 +0700

    Bump dependency (clj-stacktrace 0.2.6)

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure     "1.4.0"]
                  [org.clojure/tools.macro "0.1.2"]
-                 [clj-stacktrace          "0.2.5"]
+                 [clj-stacktrace          "0.2.6"]
                  [expectations            "1.4.48"]]
   :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
