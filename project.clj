7689d5fca738877c6c9058e1dffb759d2d677455
Peter Taoussanis <ptaoussanis@gmail.com>
2013-10-03 20:59:00 +0700

    Move `expectations` dependency to `:test` profile

--- project.clj
+++ project.clj
@@ -5,13 +5,12 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure     "1.4.0"]
                  [org.clojure/tools.macro "0.1.5"]
-                 [clj-stacktrace          "0.2.6"]
-                 [expectations            "1.4.55"]]
+                 [clj-stacktrace          "0.2.6"]]
   :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6  {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :dev  {:dependencies []}
-             :test {:dependencies []}}
+             :test {:dependencies [[expectations "1.4.56"]]}}
   :aliases {"test-all"  ["with-profile" "test,1.4:test,1.5:test,1.6" "expectations"]
             "test-auto" ["with-profile" "test" "autoexpect"]
             "start-dev" ["with-profile" "dev,test,bench" "repl" ":headless"]
