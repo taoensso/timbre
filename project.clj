5067baf1f96ba68793270d363061799abdaa8a24
Peter Taoussanis <ptaoussanis@gmail.com>
2013-11-18 14:11:22 +0700

    Deps: Clojure 1.6.0-alpha2 (for tests), lein-ancient 0.5.4

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [clj-stacktrace          "0.2.7"]]
   :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6  {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :1.6  {:dependencies [[org.clojure/clojure "1.6.0-alpha2"]]}
              :dev  {:dependencies []}
              :test {:dependencies [[expectations "1.4.56"]]}}
   :aliases {"test-all"  ["with-profile" "+test,+1.4:+test,+1.5:+test,+1.6" "expectations"]
@@ -17,7 +17,7 @@
             "codox"     ["with-profile" "+test" "doc"]}
   :plugins [[lein-expectations "0.0.8"]
             [lein-autoexpect   "1.0"]
-            [lein-ancient      "0.5.3"]
+            [lein-ancient      "0.5.4"]
             [codox             "0.6.6"]]
   :min-lein-version "2.0.0"
   :global-vars {*warn-on-reflection* true}
