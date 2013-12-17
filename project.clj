e775711355acc8abd831a2226e552edd24eac522
Peter Taoussanis <ptaoussanis@gmail.com>
2013-12-17 15:46:01 +0700

    v3.0.0-RC3 (hotfix)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/timbre "3.0.0-RC2"
+(defproject com.taoensso/timbre "3.0.0-RC3"
   :description "Clojure logging & profiling library"
   :url "https://github.com/ptaoussanis/timbre"
   :license {:name "Eclipse Public License"
