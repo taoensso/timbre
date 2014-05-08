db7fcb8214d7428c7086bea5d77cb3a0e0c16262
Angus Fletcher <afletcher@liveops.com>
2014-05-08 15:07:42 -0300

    Add doc string to appender.

--- src/taoensso/timbre/appenders/zmq.clj
+++ src/taoensso/timbre/appenders/zmq.clj
@@ -4,22 +4,31 @@
   (:require [zeromq.zmq :as zmq]
             [taoensso.timbre :as timbre]))
 
-(defn make-zmq-socket [transport address port]
-  (let [context (zmq/zcontext)]
-    (doto (zmq/socket context :push)
-      (zmq/connect (format "%s://%s:%s" transport address port)))))
+(defn make-zmq-socket [context transport address port]
+  (doto (zmq/socket context :push)
+    (zmq/connect (format "%s://%s:%d" transport address port))))
 
-(defn appender-fn [socket {:keys [ap-config output]}]
-    (zmq/send-str socket output))
+(defn appender-fn [socket poller {:keys [ap-config output]}]
+    (loop []
+      (zmq/poll poller 500)
+      (cond 
+        (zmq/check-poller poller 0 :pollout) (zmq/send-str socket output)
+        (zmq/check-poller poller 0 :pollerr) (System/exit 1)
+        :else (recur))))
 
 (defn make-zmq-appender
-  "Returns a ØMQ appender."
+  "Returns a ØMQ appender. Takes appender options and a map consisting of: 
+    transport: a string representing transport type: tcp, ipc, inproc, pgm/epgm
+    address: a string containing an address to connect to.
+    port: a number representing the port to connect to."
   [& [appender-opts {:keys [transport address port]}]]
   (let [default-appender-opts {:enabled? true
                                :min-level :error
-                               :async? true}
-        socket (make-zmq-socket transport address port)]
+                               :async? false}
+        context (zmq/zcontext)
+        socket (make-zmq-socket context transport address port)
+        poller (doto (zmq/poller context)
+                 (zmq/register socket :pollout :pollerr))]
     (merge default-appender-opts 
            appender-opts 
-           {:socket socket}   
-           {:fn (partial appender-fn socket)})))
+           {:fn (partial appender-fn socket poller)})))
