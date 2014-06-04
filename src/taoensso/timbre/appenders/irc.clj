c923698ebf8b02a7ab30ff3dd2f5478fc8485b46
Chris Truter <jeffpalentine@gmail.com>
2014-06-04 10:43:49 +0200

    Remove global conn atom
    Also no longer wrapping atom within atom (just decanting it)

--- src/taoensso/timbre/appenders/irc.clj
+++ src/taoensso/timbre/appenders/irc.clj
@@ -5,9 +5,13 @@
             [irclj.core      :as irc]
             [taoensso.timbre :as timbre]))
 
-(def conn (atom nil))
+(def default-appender-opts
+  {:enabled?  true
+   :min-level :info
+   :async?     true
+   :prefix-fn (fn [args] (-> args :level name str/upper-case))})
 
-(defn connect [{:keys [host port pass nick user name chan]
+(defn- connect [{:keys [host port pass nick user name chan]
                 :or   {port 6667}}]
   (let [conn (irc/connect host port nick
                           :username  user
@@ -17,17 +21,30 @@
     (irc/join conn chan)
     conn))
 
-(defn ensure-conn [conf]
-  (or @conn
-      (reset! conn (connect conf))))
+(defn- ensure-conn [conn conf]
+  (if-not @conn
+    (reset! conn @(connect conf))))
 
-(defn send-message [{:keys [prefix throwable message chan] :as config}]
-  (let [conn (ensure-conn config)
-        lines (-> (str message (timbre/stacktrace throwable "\n"))
-                  (str/split #"\n"))]
+(defn- send-message [conn {:keys [prefix throwable message chan] :as config}]
+  (ensure-conn conn config)
+  (let [output (str message (timbre/stacktrace throwable "\n"))
+        lines  (str/split output #"\n")]
     (irc/message conn chan prefix (first lines))
     (doseq [line (rest lines)]
       (irc/message conn chan ">" line))))
+
+(defn- make-appender-fn [irc-config conn]
+  (fn [{:keys [ap-config prefix throwable message]}]
+    (prn ap-config)
+    (when-let [irc-config (or irc-config (:irc ap-config))]
+      (send-message
+       conn
+       (assoc irc-config
+         :prefix    prefix
+         :message   message
+         :throwable throwable)))))
+
+;;; Public
 
 (defn make-irc-appender
   "Sends IRC messages using irc.
@@ -35,21 +52,12 @@
    {:host \"irc.example.org\" :port 6667 :nick \"logger\"
     :name \"My Logger\" :chan \"#logs\"}"
   [& [appender-opts {:keys [irc-config]}]]
-  (let [default-appender-opts
-        {:enabled?  true
-         :min-level :info
-         :async?     true
-         :prefix-fn (fn [args] (-> args :level name str/upper-case))}]
-    (merge default-appender-opts appender-opts
-           {:doc (:doc (meta #'make-irc-appender))
-            :fn
-            (fn [{:keys [ap-config prefix throwable message]}]
-              (when-let [irc-config (or irc-config (:irc ap-config))]
-                (send-message
-                 (assoc irc-config
-                   :prefix    prefix
-                   :throwable throwable
-                   :message   message))))})))
+  (let [conn (atom nil)]
+    (merge default-appender-opts
+           appender-opts
+           {:conn conn
+            :doc (:doc (meta #'make-irc-appender))
+            :fn  (make-appender-fn irc-config conn)})))
 
-(def irc-appender
+(def irc-appender "DEPRECATED: Use `make-irc-appender` instead."
   (make-irc-appender))
--- test/taoensso/timbre/appenders/irc_test.clj
+++ test/taoensso/timbre/appenders/irc_test.clj
@@ -4,8 +4,6 @@
             [irclj.core :as irc]
             [taoensso.timbre.appenders.irc :refer :all]))
 
-(defonce test-conn (atom {}))
-
 (def test-config
   {:irc {:host "irc.example.org"
          :nick "lazylog"
@@ -20,17 +18,16 @@
 (deftest test-irc
   (with-redefs [irc/connect
                 (fn [host port nick & attrs]
-                  (reset! test-conn
-                          {:rooms []
-                           :messages []
-                           :config (apply hash-map
-                                          :host host
-                                          :port port
-                                          :nick nick attrs)})
-                  test-conn)
+                  (atom {:rooms []
+                         :messages []
+                         :config (apply hash-map
+                                        :host host
+                                        :port port
+                                        :nick nick attrs)}))
 
                 irc/join
-                (fn [conn chan] (swap! conn update-in [:rooms] conj chan))
+                (fn [conn chan]
+                  (swap! conn update-in [:rooms] conj chan))
 
                 irc/message
                 (fn [conn chan & strs]
@@ -40,18 +37,39 @@
                                :message (str/join " " strs)}))]
 
     (testing "default constructor"
-      (reset! conn nil)
+      (let [appender (make-irc-appender)]
+        ((:fn appender) {:ap-config test-config
+                         :prefix    "[prefix]"
+                         :message   "the message"})
+        (is (= @(:conn appender)
+               {:rooms ["#logs"],
+                :messages [{:target "#logs", :message "[prefix] the message"}],
+                :config
+                {:nick "lazylog",
+                 :username "lazare",
+                 :port 6667,
+                 :callbacks {},
+                 :host "irc.example.org",
+                 :pass "s3kret",
+                 :real-name "Lazare Logerus"}}))))
+
+    (testing "reusing appender"
+      (reset! (:conn irc-appender) nil)
       ((:fn irc-appender) {:ap-config test-config
                            :prefix    "[prefix]"
                            :message   "the message"})
-      (= @test-conn
-         {:rooms ["#logs"],
-          :messages [{:target "#logs", :message "[prefix] the message"}],
-          :config
-          {:nick "lazylog",
-           :username "lazare",
-           :port 6667,
-           :callbacks {},
-           :host "irc.example.org",
-           :pass "s3kret",
-           :real-name "Lazare Logerus"}}))))
+      ((:fn irc-appender) {:ap-config test-config
+                           :prefix    "[prefix]"
+                           :message   "the next message"})
+      (is (= @(:conn irc-appender)
+             {:rooms ["#logs"],
+              :messages [{:target "#logs", :message "[prefix] the message"}
+                         {:target "#logs", :message "[prefix] the next message"}],
+              :config
+              {:nick "lazylog",
+               :username "lazare",
+               :port 6667,
+               :callbacks {},
+               :host "irc.example.org",
+               :pass "s3kret",
+               :real-name "Lazare Logerus"}})))))
