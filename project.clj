10690f5731595a9ddec13e987fa638453a722e50
Peter Taoussanis <ptaoussanis@gmail.com>
2015-09-26 12:24:40 +0700

    v4.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/timbre "4.1.1"
+(defproject com.taoensso/timbre "4.1.2"
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Clojure/Script logging & profiling library"
   :url "https://github.com/ptaoussanis/timbre"
