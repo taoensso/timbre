f8961d7d70fd6353566ae9a653f5fab8e9a1c6af
Samuel Otter <samuel.otter@klarna.com>
2017-01-13 11:01:29 +0100

    Add event-fn for injecting stuff into events

--- src/taoensso/timbre/appenders/3rd_party/sentry.clj
+++ src/taoensso/timbre/appenders/3rd_party/sentry.clj
@@ -23,26 +23,30 @@
   opts may contain additional attributes that will be passed on to sentry. See
   https://docs.sentry.io/clientdev/attributes/ for more information"
   [dsn & [opts]]
-  (let [base-event (->> (select-keys opts [:tags :environment :release :modules])
+  (let [{:keys [event-fn] :or {event-fn identity}} opts
+        base-event (->> (select-keys opts [:tags :environment :release :modules])
                         (filter (comp not nil? second))
                         (into {}))]
        {:enabled?   true
-        :async?     false
-        :min-level  :warn
+        :async?     true
+        :min-level  :warn ; Sentry supports info and debug as well but warning
+                          ; is a reasonable default
         :rate-limit nil
         :output-fn  :inherit
         :fn
         (fn [data]
           (let [{:keys [instant level output_ ?err msg_ ?ns-str context]} data
-                event (merge base-event
-                             {:message (force msg_)
-                              :logger  ?ns-str
-                              :level   (get levels level :warning)}
-                             (when context
-                               {:extra context}))]
-            (raven/capture dsn (if ?err
-                                 (interfaces/stacktrace event ?err)
-                                 event))))}))
+                event (as-> base-event event
+                        (merge event {:message (force msg_)
+                                      :logger  ?ns-str
+                                      :level   (get levels level :warning)}
+                               (when context
+                                 {:extra context}))
+                        (if ?err
+                          (interfaces/stacktrace event ?err)
+                          event)
+                        (event-fn event))]
+            (raven/capture dsn event)))}))
 
 (comment
   ;; Create an example appender with default options:
