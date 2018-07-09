33baecb3c947c6ec22c70efe9968b6f879a42a88
John Jacobsen <jjacobsen@opploans.com>
2018-07-09 17:22:32 -0500

    Revert "Reverted project version in prep for pull request"
    This reverts commit 95604404baf4cb7cf60c7558710578d04f75d6ae.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/timbre "4.10.0"
+(defproject opploans/timbre "4.10.1"
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Pure Clojure/Script logging library"
   :url "https://github.com/ptaoussanis/timbre"
