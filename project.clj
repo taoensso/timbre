5d37c3deff444e5beada7acd2984e58d03896e94
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-02-15 09:23:17 +0000

    move forward clojure profiles

--- project.clj
+++ project.clj
@@ -23,11 +23,10 @@
   :profiles
   {;; :default [:base :system :user :provided :dev]
    :server-jvm {:jvm-opts ^:replace ["-server"]}
-   :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
-   :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
    :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9  {:dependencies [[org.clojure/clojure "1.9.0-alpha10"]]}
+   :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
+   :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}
    :test
    {:dependencies
     [[org.clojure/tools.logging "0.3.1"]
