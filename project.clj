a18c2d830641d9130ddb30f6e809bbef49f675fd
Peter Taoussanis <ptaoussanis@gmail.com>
2020-08-29 18:57:23 +0200

    Update project.clj template

--- project.clj
+++ project.clj
@@ -11,90 +11,68 @@
                 *assert*             true}
 
   :dependencies
-  [[org.clojure/clojure "1.5.1" :scope "provided"]
-   [com.taoensso/encore "2.126.2"]
+  [[com.taoensso/encore "2.126.2"]
    [io.aviso/pretty     "0.1.37"]]
 
   :plugins
-  [[lein-pprint  "1.3.2"]
-   [lein-ancient "0.6.15"]
-   [lein-codox   "0.10.7"]]
+  [[lein-pprint    "1.3.2"]
+   [lein-ancient   "0.6.15"]
+   [lein-codox     "0.10.7"]
+   [lein-cljsbuild "1.1.8"]]
 
   :profiles
   {;; :default [:base :system :user :provided :dev]
    :server-jvm {:jvm-opts ^:replace ["-server"]}
-   :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
-   :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
-   :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
-   :1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}
-   :test
-   {:dependencies
-    [[org.clojure/tools.logging "1.1.0"]
+   :provided {:dependencies [[org.clojure/clojure       "1.5.1"]
+                             [org.clojure/clojurescript "1.10.773"]]}
+   :1.5      {:dependencies [[org.clojure/clojure "1.5.1"]]}
+   :1.6      {:dependencies [[org.clojure/clojure "1.6.0"]]}
+   :1.7      {:dependencies [[org.clojure/clojure "1.7.0"]]}
+   :1.8      {:dependencies [[org.clojure/clojure "1.8.0"]]}
+   :1.9      {:dependencies [[org.clojure/clojure "1.9.0"]]}
+   :1.10     {:dependencies [[org.clojure/clojure "1.10.1"]]}
+   :dev      [:1.10 :test :server-jvm]
+   :test     {:dependencies
+              [[org.clojure/test.check "1.1.0"]
+               [org.clojure/tools.logging "1.1.0"]
 
-     ;;; Appender deps
-     [com.taoensso/nippy      "2.15.1"]
-     [com.taoensso/carmine    "2.19.1"]
-     [com.draines/postal      "2.0.3"]
-     [irclj                   "0.5.0-alpha4"]
-     [org.graylog2/gelfclient "1.5.0" :exclusions [com.fasterxml.jackson.core/jackson-core]]
-     [org.julienxx/clj-slack  "0.6.3"]
-     [org.clojure/java.jdbc   "0.7.11"]
-     [com.mchange/c3p0        "0.9.5.5"]
-     [cheshire                "5.10.0"]
-     [ymilky/franzy           "0.0.1"]
-     [com.newrelic.agent.java/newrelic-agent "6.0.0"]
-     [net.java.dev.jna/jna    "5.6.0"]
-     [raven-clj               "1.6.0"]]}
-
-   :provided {:dependencies [[org.clojure/clojurescript "1.10.773"]]}
-
-   :dev
-   [:1.10 :test :server-jvm
-    {:plugins
-     [;; These must be in :dev, Ref. https://github.com/lynaghk/cljx/issues/47:
-      [com.keminglabs/cljx "0.6.0"]
-      [lein-cljsbuild      "1.1.8"]]}]}
-
-  ;; :jar-exclusions [#"\.cljx|\.DS_Store"]
-  :source-paths ["src" "target/classes"]
-  :test-paths   ["src" "test" "target/test-classes"]
-
-  :cljx
-  {:builds
-   [{:source-paths ["src"]        :rules :clj  :output-path "target/classes"}
-    {:source-paths ["src"]        :rules :cljs :output-path "target/classes"}
-    {:source-paths ["src" "test"] :rules :clj  :output-path "target/test-classes"}
-    {:source-paths ["src" "test"] :rules :cljs :output-path "target/test-classes"}]}
+               ;;; Appender deps
+               [com.taoensso/nippy      "2.15.1"]
+               [com.taoensso/carmine    "2.19.1"]
+               [com.draines/postal      "2.0.3"]
+               [irclj                   "0.5.0-alpha4"]
+               [org.graylog2/gelfclient "1.5.0"
+                :exclusions [com.fasterxml.jackson.core/jackson-core]]
+               [org.julienxx/clj-slack  "0.6.3"]
+               [org.clojure/java.jdbc   "0.7.11"]
+               [com.mchange/c3p0        "0.9.5.5"]
+               [cheshire                "5.10.0"]
+               [ymilky/franzy           "0.0.1"]
+               [com.newrelic.agent.java/newrelic-agent "6.0.0"]
+               [net.java.dev.jna/jna    "5.6.0"]
+               [raven-clj               "1.6.0"]]}}
 
   :cljsbuild
-  {:test-commands {"node"    ["node" :node-runner "target/tests.js"]
-                   "phantom" ["phantomjs" :runner "target/tests.js"]}
+  {:test-commands
+   {"node"    ["node" :node-runner "target/main.js"]
+    "phantom" ["phantomjs" :runner "target/main.js"]}
+
    :builds
-   [{:id "main"
-     :source-paths   ["src" "target/classes"]
-     ;; :notify-command ["terminal-notifier" "-title" "cljsbuild" "-message"]
-     :compiler       {:output-to "target/main.js"
-                      :optimizations :advanced
-                      :pretty-print false}}]}
-
-  :auto-clean false
-  :prep-tasks [["cljx" "once"] "javac" "compile"]
-
-  :codox
-  {:language :clojure
-   :source-uri
-   {#"target/classes" "https://github.com/ptaoussanis/timbre/blob/v{version}/src/{classpath}x#L{line}"
-    #".*"             "https://github.com/ptaoussanis/timbre/blob/v{version}/{filepath}#L{line}"}}
+   [{:id :main
+     :source-paths ["src" "test"]
+     :compiler
+     {:output-to "target/main.js"
+      :optimizations :advanced
+      :pretty-print false}}]}
 
   :aliases
-  {"test-all"   ["do" "clean," "cljx" "once,"
-                 "with-profile" "+1.9:+1.8:+1.7:+1.6:+1.5" "test"
-                 "with-profile" "+test" "cljsbuild" "test"]
-   "build-once" ["do" "clean," "cljx" "once," "cljsbuild" "once" "main"]
+  {"build-once" ["cljsbuild" "once"]
    "deploy-lib" ["do" "build-once," "deploy" "clojars," "install"]
-   "start-dev"  ["with-profile" "+dev" "repl" ":headless"]}
+   "start-dev"  ["with-profile" "+dev" "repl" ":headless"]
+   "test-all"   ["do" "clean,"
+                 "with-profile" "+1.10:+1.9:+1.8:+1.7:+1.6:+1.5" "test"
+                 "with-profile" "+test" "cljsbuild" "test"]}
 
-  :repositories {"sonatype-oss-public"
-                 "https://oss.sonatype.org/content/groups/public/"})
+  :repositories
+  {"sonatype-oss-public"
+   "https://oss.sonatype.org/content/groups/public/"})
